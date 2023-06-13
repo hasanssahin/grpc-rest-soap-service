67d5a7b41da2fc8a639a1d34d0cc636a86c3b15a
hasanssahin <sahinhhasann@gmail.com>
2023-06-14 00:43:13 +0300

    Create database connection

---
+++ database/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+mongoose
+	.connect('mongodb://127.0.0.1:27017/dbUser')
+	.then(console.log('Database connection successful.'))
+	.catch((err) => console.log('Database connection failed: ' + err))
